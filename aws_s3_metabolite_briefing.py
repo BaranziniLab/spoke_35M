ca62cf273df52f80c2ab1c161ec718b68ae4e658
Karthik Soman <karthiksoman@neur-qrlvvhf-lt.ucsf.edu>
2023-04-06 18:06:57 -0700

    adding s3 utility file

--- aws_s3_metabolite_briefing.py
+++ aws_s3_metabolite_briefing.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 import sys
+import boto3
 
 GLOBAL_MAPPING_FILE = sys.argv[1]
 SHORTCHAIN_MAPPING_FILE = sys.argv[2]
@@ -25,4 +26,26 @@ global_short_chain_shared_merge = global_short_chain_shared_merge.rename(columns
 
 print("There are {} short chain fatty acids shared in global file".format(global_short_chain_shared_merge.short_chain_fatty_acid_name.unique().shape[0]))
 print("They are:")
-global_short_chain_shared_merge
+print(global_short_chain_shared_merge)
+
+
+cmd = "aws s3 ls s3://ic-spoke/spoke35M/"
+out = os.popen(cmd)
+out_list = out.read().split("\n")
+saved_compound_list = np.array([element for element in out_list if "Compound:inchikey:" in element])
+saved_compound_list_ = ['Compound:inchikey:' + element.split("Compound:inchikey:")[-1].replace('_dict.pickle', '') for element in saved_compound_list if "Compound:inchikey:" in element]
+
+s3_client = boto3.client('s3')
+bucket_name = 'ic-spoke'
+compounds_with_no_pagerank = []
+for node_id in saved_compound_list_:
+  object_key = 'spoke35M/{}_dict.pickle'.format(node_id)
+  response = s3_client.head_object(Bucket=bucket_name, Key=object_key)
+  if response['ContentLength'] < 222:
+    compounds_with_no_pagerank.append(node_id)
+print("There are {} Compounds with no pagerank".format(len(compounds_with_no_pagerank)))
+compounds_with_no_pagerank_df = pd.DataFrame(compounds_with_no_pagerank, columns=["node_id"])
+csv_string = compounds_with_no_pagerank_df.to_csv(index=False)
+s3_client.put_object(Bucket=bucket_name, Key="spoke35M/compounds_with_no_pagerank.csv", Body=csv_string.encode('utf-8'))
+
+
---
+++ s3_utility.py
@@ -0,0 +1,25 @@
+import boto3
+import os
+import numpy as np
+
+
+def get_saved_compoundid_from_s3():
+	cmd = "aws s3 ls s3://ic-spoke/spoke35M/"
+	out = os.popen(cmd)
+	out_list = out.read().split("\n")
+	saved_compound_list = np.array([element for element in out_list if "Compound:inchikey:" in element])
+	saved_compound_list_ = ['Compound:inchikey:' + element.split("Compound:inchikey:")[-1].replace('_dict.pickle', '') for element in saved_compound_list if "Compound:inchikey:" in element]
+	return saved_compound_list_
+
+
+def get_saved_compounds_with_no_pagerank(saved_compound_list_):
+	s3_client = boto3.client('s3')
+	bucket_name = 'ic-spoke'
+	compounds_with_no_pagerank = []
+	for node_id in saved_compound_list_:
+	  object_key = 'spoke35M/{}_dict.pickle'.format(node_id)
+	  response = s3_client.head_object(Bucket=bucket_name, Key=object_key)
+	  if response['ContentLength'] < 222:
+	    compounds_with_no_pagerank.append(node_id)
+    return compounds_with_no_pagerank
+
