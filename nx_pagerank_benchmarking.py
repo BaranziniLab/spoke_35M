9c687000da2dedb8700ff58314c047e1f3606848
ksoman <karthi.soman@gmail.com>
2023-05-03 23:21:31 -0700

    adding new script for tolerance optimization

--- nx_pagerank_benchmarking.py
+++ nx_pagerank_benchmarking.py
@@ -12,16 +12,16 @@ node_list = ['Compound:inchikey:QPIJOYAYKONXJD-ZYLBBPIUSA-N', 'Compound:inchikey
 
 
 def main():
-	with open(GRAPH_PATH, "rb") as f:
-	    G = pickle.load(f)
+    with open(GRAPH_PATH, "rb") as f:
+        G = pickle.load(f)
 
-	completion_time_list = []
-	for item in node_list:	
-		personalization = {item:1}
-		start_time = time.time()
-		personalized_pagerank = nx.pagerank(G, alpha=0.85, personalization=personalization)
-		completion_time_list.append(round((time.time()-start_time)/(60*60),2))
-	joblib.dump(completion_time_list, SAVE_PATH)
+    completion_time_list = []
+    for item in node_list:
+        personalization = {item:1}
+        start_time = time.time()
+        personalized_pagerank = nx.pagerank(G, alpha=0.85, personalization=personalization)
+        completion_time_list.append(round((time.time()-start_time)/(60*60),2))
+    joblib.dump(completion_time_list, SAVE_PATH)
 
 
 if __name__ == "__main__":
---
+++ nx_pagerank_tolerance_optimization.py
@@ -0,0 +1,41 @@
+import networkx as nx
+import sys
+import pickle
+import time
+import joblib
+
+
+GRAPH_PATH = sys.argv[1]
+max_iter = int(sys.argv[2])
+tol = float(sys.argv[3])
+
+node_list = ['Food:FOODON:03301713']
+
+N = 10
+
+
+def main():
+    with open(GRAPH_PATH, "rb") as f:
+        G = pickle.load(f)
+
+    completion_time_list = []
+    item = node_list[0]
+    personalization = {item:1}
+    start_time = time.time()
+    personalized_pagerank = nx.pagerank(G, alpha=0.85, personalization=personalization, max_iter=max_iter, tol=tol)
+    completion_time = round((time.time()-start_time)/(60),2)
+    print("PPR is completed in {} min".format(completion_time))
+    
+    filtered_dict = {k:v for k,v in personalized_pagerank.items() if "Compound:inchikey" in k}
+    top_N_compounds = sorted(filtered_dict.items(), key=lambda x: x[1], reverse=True)[:N]
+    top_N_compounds_ids = [x[0] for x in top_N_compounds]
+    compound_neighbors = [n for n in G.neighbors(node_list[0]) if n.startswith('Compound:inchikey')]
+    top_compound_neighbors = set(top_N_compounds_ids).intersection(set(compound_neighbors))
+    print("Out of {} top compounds in PPR vector, {} compounds are first nbrs of the selected node for which PPR is computed".format(N, len(top_compound_neighbors)))
+
+            
+    
+
+
+if __name__ == "__main__":
+    main()
