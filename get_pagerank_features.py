15194ff75caa84dbeef487ae8d6fa7e3a9715ef2
Karthik Soman <karthiksoman@neur-qrlvvhf-lt.ucsf.edu>
2023-05-09 13:30:26 -0700

    changed features to dict

--- get_pagerank_features.py
+++ get_pagerank_features.py
@@ -20,11 +20,12 @@ def main():
 	for item in node_list:
 		personalization = {item:1}
 		personalized_pagerank = nx.pagerank(G, alpha=0.85, personalization=personalization, max_iter=200, tol=1e-12)
-	features = np.array(list(personalized_pagerank.keys()))
+	features = {}
+	features["features"] = np.array(list(personalized_pagerank.keys()))
 	binary_data = pickle.dumps(features)
 	s3_client = boto3.client('s3')
 	bucket_name = 'ic-spoke'
-	object_key = 'spoke35M/spoke35M_converged_ppr/ppr_features_array.pickle'
+	object_key = 'spoke35M/spoke35M_converged_ppr/ppr_features_dict.pickle'
 	s3_client.put_object(Bucket=bucket_name, Key=object_key, Body=binary_data)
 	s3_client.close()
 	completion_time = round((time.time()-start_time)/(60),2)
