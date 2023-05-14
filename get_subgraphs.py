bff1cc1ce3270934862c15cb8fee58190ad36f49
Karthik Soman <karthi.soman@gmail.com>
2023-05-14 15:50:36 -0700

    subgraph script is amended to include path extraction for non-targeted compound types

--- get_subgraphs.py
+++ get_subgraphs.py
@@ -27,10 +27,12 @@ def main():
 	start_time = time.time()
 	with open(GRAPH_PATH, "rb") as f:
 	    G = pickle.load(f)
-	starting_compound_nodes = get_starting_compound_nodes()
-	salient_intermediate_nodes_proximal_to_MS = get_salient_intermediate_nodes_proximal_to_MS()
-	compound_to_intermediate_paths = get_paths_from_compound_to_intermediate_nodes(starting_compound_nodes, salient_intermediate_nodes_proximal_to_MS)
-	intermediate_nodes_to_MS_node_paths = get_paths_from_intermediate_nodes_to_MS_node(salient_intermediate_nodes_proximal_to_MS, destination_disease_node)
+    salient_intermediate_nodes_proximal_to_MS = get_salient_intermediate_nodes_proximal_to_MS()
+    intermediate_nodes_to_MS_node_paths = get_paths_from_intermediate_nodes_to_MS_node(salient_intermediate_nodes_proximal_to_MS, destination_disease_node)
+    compound_to_intermediate_paths = []
+    if compound_type == "targeted":
+		starting_compound_nodes = get_starting_compound_nodes()	
+		compound_to_intermediate_paths = get_paths_from_compound_to_intermediate_nodes(starting_compound_nodes, salient_intermediate_nodes_proximal_to_MS)	
 	extracted_path = {}
 	extracted_path["compound_to_intermediate"] = compound_to_intermediate_paths
 	extracted_path["intermediate_to_MS"] = intermediate_nodes_to_MS_node_paths
