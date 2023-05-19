40ccf81bb5634aa43a38812018578237aafef1ab
Karthik Soman <karthi.soman@gmail.com>
2023-05-19 16:52:58 -0700

    get_pagerank_features.py

--- get_pagerank_features.py
+++ get_pagerank_features.py
@@ -6,6 +6,7 @@ import os
 import numpy as np
 import time
 import gzip
+import pandas as pd
 
 
 GRAPH_PATH = sys.argv[1]
@@ -25,12 +26,21 @@ def main():
 		personalized_pagerank = nx.pagerank(G, alpha=0.85, personalization=personalization, max_iter=200, tol=1e-12)
 	features = {}
 	features["features"] = np.array(list(personalized_pagerank.keys()))
-	binary_data = pickle.dumps(features)
-	compressed_data = gzip.compress(binary_data)
-	s3_client = boto3.client('s3')
-	bucket_name = BUCKET_NAME
-	object_key = '{}/ppr_features_dict_gzip_compressed'.format(FILE_LOCATION)
-	s3_client.put_object(Bucket=bucket_name, Key=object_key, Body=compressed_data)
+
+	features = pd.DataFrame(np.array(list(personalized_pagerank.keys())), columns=["node_id"])
+	features.loc[:, "node_type"] = features.node_id.apply(lambda x:x.split(":")[0])
+	features.node_id = features.node_id.apply(lambda x:":".join(x.split(":")[1:]))
+	csv_data = features.to_csv(index=False)
+	file_name = "{}/spoke35M_ppr_features.csv".format(FILE_LOCATION)
+	s3_client.put_object(Body=csv_data, Bucket=BUCKET_NAME, Key=file_name)
+
+	# binary_data = pickle.dumps(features)
+	# compressed_data = gzip.compress(binary_data)
+	# s3_client = boto3.client('s3')
+	# bucket_name = BUCKET_NAME
+	# object_key = '{}/ppr_features_dict_gzip_compressed'.format(FILE_LOCATION)
+	# s3_client.put_object(Bucket=bucket_name, Key=object_key, Body=compressed_data)
+	
 	s3_client.close()
 	completion_time = round((time.time()-start_time)/(60),2)
 	print("Features are extracted from PPR and then stored in S3 in {} min!".format(completion_time))
