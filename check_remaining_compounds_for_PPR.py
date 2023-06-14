cbb187b7c4b76229839d76569183530e3d75ff15
Karthik Soman <karthi.soman@gmail.com>
2023-06-13 20:06:08 -0700

    amended checking compound script

--- check_remaining_compounds_for_PPR.py
+++ check_remaining_compounds_for_PPR.py
@@ -4,16 +4,17 @@ import pandas as pd
 import sys
 
 MAPPING_FILE = sys.argv[1]
-bucket_location = sys.argv[2]
+IDENTIFIER_COLUMN = sys.argv[2]
+bucket_location = sys.argv[3]
 
 mapping_file_df = pd.read_csv(MAPPING_FILE)
-mapping_file_df["spoke_identifer"] = "Compound:" + mapping_file_df["spoke_identifer"]
+mapping_file_df[IDENTIFIER_COLUMN] = "Compound:" + mapping_file_df[IDENTIFIER_COLUMN]
 cmd = "aws s3 ls s3://{}".format(bucket_location)
 out = os.popen(cmd)
 out_list = out.read().split("\n")
 saved_compound_list = np.array([element for element in out_list if "Compound:inchikey:" in element])
 saved_compound_list_ = ['Compound:inchikey:' + element.split("Compound:inchikey:")[-1].replace('_dict.pickle', '') for element in saved_compound_list if "Compound:inchikey:" in element]
-node_list = mapping_file_df["spoke_identifer"].unique()
+node_list = mapping_file_df[IDENTIFIER_COLUMN].unique()
 node_list = list(set(node_list) - set(saved_compound_list_))
 
 print("There are {} compounds left for the PPR to get computed".format(len(node_list)))
