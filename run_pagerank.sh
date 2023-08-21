6ba4b72dcbf62d389bf0e470a7ffb89a81dbd3a6
Karthik Soman <karthi.soman@gmail.com>
2023-08-21 14:29:09 -0700

    removed spaces in run bash script

--- run_pagerank.sh
+++ run_pagerank.sh
@@ -1,10 +1,10 @@
-GRAPH_PATH = "../ncats/data/spoke_graph_ncats_version_2021_02_07.gpickle"
-MAPPING_FILE = "../ncats/data/genes.csv"
-IDENTIFIER_COLUMN = "identifier"
-NCORES = 80
-bucket_name = "ic-spoke"
-sublocation = "spoke35M/ncats/ppr_vectors"
-check_existing_compounds = 0
+GRAPH_PATH="../ncats/data/spoke_graph_ncats_version_2021_02_07.gpickle"
+MAPPING_FILE="../ncats/data/genes.csv"
+IDENTIFIER_COLUMN="identifier"
+NCORES=80
+bucket_name="ic-spoke"
+sublocation="spoke35M/ncats/ppr_vectors"
+check_existing_compounds=0
 
 
 conda run -n pagerank python nx_pagerank_2.py "$GRAPH_PATH" "$MAPPING_FILE" "$IDENTIFIER_COLUMN" "$NCORES" "$bucket_name" "$sublocation" "$check_existing_compounds" >> "logs/run_pagerank.log" 2>&1 &
