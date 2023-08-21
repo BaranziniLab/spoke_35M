f42a77469ea7be3e1125154005599768ed4460ba
Karthik Soman <karthi.soman@gmail.com>
2023-08-21 14:26:13 -0700

    amended the log file name in the run bash script

--- run_pagerank.sh
+++ run_pagerank.sh
@@ -7,6 +7,6 @@ sublocation = "spoke35M/ncats/ppr_vectors"
 check_existing_compounds = 0
 
 
-conda run -n pagerank python nx_pagerank_2.py "$GRAPH_PATH" "$MAPPING_FILE" "$IDENTIFIER_COLUMN" "$NCORES" "$bucket_name" "$sublocation" "$check_existing_compounds" >> "logs/run_script.log" 2>&1 &
+conda run -n pagerank python nx_pagerank_2.py "$GRAPH_PATH" "$MAPPING_FILE" "$IDENTIFIER_COLUMN" "$NCORES" "$bucket_name" "$sublocation" "$check_existing_compounds" >> "logs/run_pagerank.log" 2>&1 &
 wait
 echo "Completed computing PPR vectors"
