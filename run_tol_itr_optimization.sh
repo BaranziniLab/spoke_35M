226ca799e7c27ddc32896f36934107984e570dd6
Karthik Soman <karthi.soman@gmail.com>
2023-09-08 00:43:10 -0700

    adding a shell script to run tol and itr optimization

---
+++ run_tol_itr_optimization.sh
@@ -0,0 +1,9 @@
+GRAPH_PATH="../spoke41M_data/spoke_41M.gpickle"
+max_iter=200
+tol=1e-18
+NODETYPE_SEPARATOR="|"
+
+
+conda run -n pagerank python nx_pagerank_tolerance_optimization.py "$GRAPH_PATH" "$max_iter" "$tol" "$NODETYPE_SEPARATOR" >> "logs/tol_opt_41M_18.log" 2>&1 &
+wait
+echo "Completed computing PPR vectors"
