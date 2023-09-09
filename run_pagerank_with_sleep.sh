8d1724543aafaf35f3554e0f113c0cea4a45eecc
Karthik Soman <karthi.soman@gmail.com>
2023-09-09 10:45:43 -0700

    adding bash script for pagerank that runs with sleep

---
+++ run_pagerank_with_sleep.sh
@@ -0,0 +1,16 @@
+GRAPH_PATH="../spoke41M_data/spoke_41M.gpickle"
+MAPPING_FILE="../spoke41M_data/priority_genes.csv"
+IDENTIFIER_COLUMN="identifier"
+NCORES=25
+bucket_name="ic-spoke"
+sublocation="spoke41M/ppr_vectors/genes"
+check_any_saved_nodes=1
+NODE_TYPE="Gene"
+NODE_TYPE_SEPERATOR="|"
+
+while true; do
+  conda run -n pagerank python nx_pagerank_2.py "$GRAPH_PATH" "$MAPPING_FILE" "$IDENTIFIER_COLUMN" "$NCORES" "$bucket_name" "$sublocation" "$check_any_saved_nodes" "$NODE_TYPE" "$NODE_TYPE_SEPERATOR" >> "logs/run_pagerank_with_sleep.log" 2>&1 &
+  sleep 2h
+  pkill -f "python nx_pagerank_2.py"
+  sleep 60
+done
