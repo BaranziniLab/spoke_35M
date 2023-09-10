a2c503f4a701df68b8a257386e03c5b350d962e0
Karthik Soman <karthi.soman@gmail.com>
2023-09-10 15:30:47 -0700

    amended the mapping file name in the pagerank_2 shell script

--- run_pagerank_with_sleep.sh
+++ run_pagerank_with_sleep.sh
@@ -1,11 +1,11 @@
 GRAPH_PATH="../spoke41M_data/spoke_41M.gpickle"
-MAPPING_FILE="../spoke41M_data/priority_genes.csv"
+MAPPING_FILE="../spoke41M_data/all_genes.csv"
 IDENTIFIER_COLUMN="identifier"
 NCORES=15
 bucket_name="ic-spoke"
 sublocation="spoke41M/ppr_vectors/genes"
 check_any_saved_nodes=1
-NODE_TYPE="Gene"
+NODE_TYPE_TO_CHECK="Gene"
 NODE_TYPE_SEPERATOR="|"
 
 start_time=$(date +%s)
@@ -19,7 +19,7 @@ while true; do
   else
     echo "Number of PPR vectors in S3: $num_files, Total nodes in mapping file: $total_rows. They do not match and hence continuing with PPR computation for the remaining nodes ..."
   fi
-  conda run -n pagerank python nx_pagerank_2.py "$GRAPH_PATH" "$MAPPING_FILE" "$IDENTIFIER_COLUMN" "$NCORES" "$bucket_name" "$sublocation" "$check_any_saved_nodes" "$NODE_TYPE" "$NODE_TYPE_SEPERATOR" >> "logs/run_pagerank_with_sleep.log" 2>&1 &
+  conda run -n pagerank python nx_pagerank_2.py "$GRAPH_PATH" "$MAPPING_FILE" "$IDENTIFIER_COLUMN" "$NCORES" "$bucket_name" "$sublocation" "$check_any_saved_nodes" "$NODE_TYPE_TO_CHECK" "$NODE_TYPE_SEPERATOR" >> "logs/run_pagerank_with_sleep.log" 2>&1 &
   sleep 2h
   pkill -f "python nx_pagerank_2.py"
   sleep 60
